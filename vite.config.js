85e1f82c97e3a59fd5c12a78b97f3c153346cd67
daruma30610 <sinden30610@gmail.com>
2025-06-01 22:58:25 +0900

    fix: GitHub Pagesが見れないのを修正

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: '/password_generation/',
+  base: '/password_generation_20250501/',
 })
